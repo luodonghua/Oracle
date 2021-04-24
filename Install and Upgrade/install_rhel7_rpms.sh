ff5d6769e09d49a46c789e63329a50627b20d5dc
Luo Donghua <luodonghua@gmail.com>
2021-04-24 20:00:25 +0800

    Create install_rhel7_rpms.sh
    # Package list reference to RHEL 7 Requirements
    Tested on AWS EC2 Linux AMI as well

---
+++ Install and Upgrade/install_rhel7_rpms.sh
@@ -0,0 +1,28 @@
+# Package list reference to RHEL 7 Requirements
+yum install -y                     \
+bc                                 \
+binutils                           \
+compat-libcap1                     \
+compat-libstdc++-33                \
+elfutils-libelf                    \
+elfutils-libelf-devel              \
+fontconfig-devel                   \
+glibc                              \
+glibc-devel                        \
+ksh                                \
+libaio                             \
+libaio-devel                       \
+libX11                             \
+libXau                             \
+libXi                              \
+libXtst                            \
+libXrender                         \
+libXrender-devel                   \
+libgcc                             \
+libstdc++                          \
+libstdc++-devel                    \
+libxcb                             \
+make                               \
+smartmontools                      \
+sysstat                            \
+net-tools
