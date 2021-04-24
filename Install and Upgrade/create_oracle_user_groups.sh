fdccf03a90b70f5a8c80a7604cbfb061c7e14779
Luo Donghua <luodonghua@gmail.com>
2021-04-24 20:01:00 +0800

    Create create_oracle_user_groups.sh

---
+++ Install and Upgrade/create_oracle_user_groups.sh
@@ -0,0 +1,13 @@
+
+/usr/sbin/groupadd -g 54321 oinstall
+/usr/sbin/groupadd -g 54322 dba
+/usr/sbin/groupadd -g 54323 oper
+/usr/sbin/groupadd -g 54324 backupdba
+/usr/sbin/groupadd -g 54325 dgdba
+/usr/sbin/groupadd -g 54326 kmdba
+/usr/sbin/groupadd -g 54327 asmdba
+/usr/sbin/groupadd -g 54328 asmoper
+/usr/sbin/groupadd -g 54329 asmadmin
+/usr/sbin/groupadd -g 54330 racdba
+
+/usr/sbin/useradd -u 54321 -g oinstall -G dba,oper,backupdba,dgdba,kmdba,asmdba,racdba -m -d /home/oracle oracle
